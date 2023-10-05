4a2cbb10192a756ed770397e1d60c8aded0164aa
IsNotRicardo <desousa.itsnotricardo@gmail.com>
2023-10-05 16:27:16 +0300

    Start of the game

---
+++ user.py
@@ -0,0 +1,121 @@
+import os
+
+import game
+
+
+# This function asks for the screen_name and checks if it already exists
+def username():
+    return False
+
+
+# This function changes the settings that can only have one option
+def change_setting(opt_list, value):
+    choice = choose_option() - 1
+
+    if 0 <= choice <= value - 1:
+        for num in range(0, value):
+            if num == choice:
+                opt_list[num] = 'X'
+            else:
+                opt_list[num] = ' '
+        return choice
+    else:
+        print("Invalid option!")
+        return -1
+
+
+# This generic function asks the user to select an option
+def choose_option():
+    while True:
+        try:
+            option = int(input("Select your option: "))
+        except ValueError:
+            print("Must be an integer!\n")
+        else:
+            break
+    return option
+
+
+# This function determines if the user can start the game
+def start_game(diff, dist):
+    if 'X' in diff and 'X' in dist:
+        print("Do you wish to start the game?\n"
+              "1. Yes\n"
+              "2. No\n")
+        if choose_option() == 1:
+            """
+            
+            Start the game
+            
+            """
+        else:
+            print("Returning to settings...\n")
+    else:
+        print("You must select a Difficulty and a Distance first!")
+
+
+# This function is where the user chooses the settings of a new game
+def new_game():
+    difficulty, distance, extra = [' '] * 3, [' '] * 3, [' '] * 2
+
+    while True:
+        print("\nGAME SETTINGS:\n")
+        print("— Game Difficulty:\n"
+              f"Easy[{difficulty[0]}]     Normal[{difficulty[1]}]     Hard[{difficulty[2]}]\n")
+        print("— Target Distance:\n"
+              f"Near[{distance[0]}]     Average[{distance[1]}]     Far[{distance[2]}]\n")
+        print("— Extra Settings: (optional):\n"
+              f"Overwater Modifier[{extra[0]}]     Hel Mode[{extra[1]}]\n")
+
+        print("Change options: \n"
+              "1. Start Game\n"
+              "2. Difficulty\n"
+              "3. Distance\n"
+              "4. Modifiers (WIP)\n"
+              "5. Quit\n")
+
+        match choose_option():
+            case 1:
+                start_game(difficulty, distance)
+            case 2:
+                print("Change difficulty: \n"
+                      "1. Easy\n"
+                      "2. Normal\n"
+                      "3. Hard\n")
+
+                if change_setting(difficulty, len(difficulty)) != -1:
+                    continue
+            case 3:
+                print("Change distance: \n"
+                      "1. Near\n"
+                      "2. Average\n"
+                      "3. Far\n")
+
+                if change_setting(distance, len(distance)) != -1:
+                    continue
+            case 4:
+                print("Extra settings: \n"
+                      "1. Overwater Modifier\n"
+                      "2. Hel Mode\n")
+
+                choice_extra = choose_option() - 1
+                if 0 <= choice_extra <= len(extra) - 1:
+                    if extra[choice_extra] == ' ':
+                        extra[choice_extra] = 'X'
+                    else:
+                        extra[choice_extra] = ' '
+            case 5:
+                quit()
+            case _:
+                print("Invalid option!")
+
+
+# Work in progress, needs more information
+def main():
+    if not username():
+        new_game()
+    else:
+        game.navigation()
+
+
+main()
